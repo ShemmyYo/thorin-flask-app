006f4d75b98226d52a94a313c61ba37aceee486d
ShemmyYo <rabiliamemo@gmail.com>
2023-02-07 17:19:56 +0000

    EOD

--- run.py
+++ run.py
@@ -1,11 +1,21 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World"
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html", page_title="About")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html", page_title="Contact")
 
 
 if __name__ == "__main__":
